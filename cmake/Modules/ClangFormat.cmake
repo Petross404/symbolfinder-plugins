9454245c403b58691f24c2b6310d426ea8286aab
Petros S <petross404@gmail.com>
2024-02-10 18:39:46 +0200

    Update ClangFormat.cmake

--- cmake/Modules/ClangFormat.cmake
+++ cmake/Modules/ClangFormat.cmake
@@ -5,13 +5,13 @@ function(handle_dependency_clang_format)
 		#file(GLOB_RECURSE ALL_CXX_SOURCE_FILES
 			#${PROJECT_SOURCE_DIR}/*.[ch]pp)
 
-		add_custom_command(OUTPUT clang_format_
+		add_custom_command(OUTPUT pclang_format_
 			COMMAND		${CMAKE_SOURCE_DIR}/tools/clang-format.sh
 			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 			COMMENT		"Running clang-format.sh. Please wait until this job is finished.")
 
-		add_custom_target(clang_format
-			DEPENDS clang_format_
+		add_custom_target(plugins_clang_format
+			DEPENDS pclang_format_
 			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR})
 	else()
 		message(WARNING "CMake failed to create a target for clang-format. Is clang-format installed?")
